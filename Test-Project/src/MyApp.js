ed039a51108b8030aa6dc1854aea2c64bd5f4afb
VaibhavB18 <142375805+VaibhavB18@users.noreply.github.com>
2024-02-18 15:34:56 +0530

    Test-Project
    Test-Project

---
+++ Test-Project/src/MyApp.js
@@ -0,0 +1,13 @@
+import './MyApp.css';
+import MyCard from './components/MyCard';
+
+
+function MyApp() {
+  return (
+    <div className='MyApp'>
+     <MyCard />
+    </div>
+  );
+}
+
+export default MyApp;
